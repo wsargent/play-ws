5bc2cbfd6e386e96771f65e9694641b429df5b2b
Will Sargent <will.sargent@gmail.com>
2016-12-20 21:08:37 -0800

    WIP

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,10 +5,10 @@ object Dependencies {
 
   // Latest snapshot is in
   // https://oss.sonatype.org/content/repositories/snapshots/com/typesafe/play/play_2.11/maven-metadata.xml
-  //val PlayVersion = playVersion("2.6.0-2016-12-11-1904442-SNAPSHOT")
+  // val PlayVersion = playVersion("2.6.0-2016-12-11-1904442-SNAPSHOT")
   // val PlayVersion = playVersion("2.5.10")
   val PlayVersion = playVersion("2.6.0-SNAPSHOT")
-
+  
   val specsVersion = "3.8.6"
   val specsBuild = Seq(
     "specs2-core",
@@ -18,7 +18,7 @@ object Dependencies {
 
   val logback = "ch.qos.logback" % "logback-classic" % "1.1.7"
 
-  val playJsonVersion = "2.6.0-SNAPSHOT"
+  val playJsonVersion = PlayVersion
   val playJson = Seq(
     "com.typesafe.play" %% "play-json" % playJsonVersion,
     "com.typesafe.play" %% "play-functional" % playJsonVersion
